299f7955e60602becde9ada8a7077827a3353523
Oksana Komarnytska <okazimiriv@gmail.com>
2023-02-17 22:17:39 +0100

    task-16

--- js/blend-1/tasks.js
+++ js/blend-1/tasks.js
@@ -252,36 +252,36 @@ if (question === "ecmascript") {
 //TODO: 16 ===================================
 // Перевірити два масиву і дізнатися, чи вони рівні
 
-const arr1 = [1, 4, 6, 'color', 324, 232, 'list', 11, 9, 'dream', 34, 0, -30];
-const arr2 = [6, 'dream', -30, 11, 9, 1, 324, 34, 'color', 4, 232, 0, 'list'];
+// const arr1 = [1, 4, 6, 'color', 324, 232, 'list', 11, 9, 'dream', 34, 0, -30];
+// const arr2 = [6, 'dream', -30, 11, 9, 1, 324, 34, 'color', 4, 232, 0, 'list'];
 
-const arr3 = [4, 232, 6, -30, 'color', 324, 'list', 1, 11, 9, 'dream', 34, 0];
-const arr4 = ['color', 6, -30, 11, 9, 1, 'dream', 324, 34, 4, 232, 0, 'list'];
+// const arr3 = [4, 232, 6, -30, 'color', 324, 'list', 1, 11, 9, 'dream', 34, 0];
+// const arr4 = ['color', 6, -30, 11, 9, 1, 'dream', 324, 34, 4, 232, 0, 'list'];
 
-const arr5 = [1, 4, 6, 'color', 'list', 11, 9, 'dream', 34, 0, -30, 'lesson'];
-const arr6 = [6, 324, 'dream', -30, 9, 8, 34, 'color', 4, 232, 0, 'list', 11];
+// const arr5 = [1, 4, 6, 'color', 'list', 11, 9, 'dream', 34, 0, -30, 'lesson'];
+// const arr6 = [6, 324, 'dream', -30, 9, 8, 34, 'color', 4, 232, 0, 'list', 11];
 
-const arr7 = [1, 4, 6, 'color', 324, 232, 'list', 11, 9, 'dream', 34, 0, -30];
-const arr8 = [6, 'dream', -30, 10, 9, 1, 324, 34, 'color', 4, 232, 0, 'list'];
+// const arr7 = [1, 4, 6, 'color', 324, 232, 'list', 11, 9, 'dream', 34, 0, -30];
+// const arr8 = [6, 'dream', -30, 10, 9, 1, 324, 34, 'color', 4, 232, 0, 'list'];
 
-console.log('arr1 -',arr7.length);
-console.log('arr2 -', arr8.length);
+// console.log('arr1 -',arr7.length);
+// console.log('arr2 -', arr8.length);
         
-function compareArrays(arr1, arr2) {
+// function compareArrays(arr1, arr2) {
      
-    if (arr1.length !== arr2.length) { 
-        return false;
-    }     
+//     if (arr1.length !== arr2.length) { 
+//         return false;
+//     }     
     
-     for (let i = 0; i < arr1.length; i += 1){
-         if (!arr2.includes(arr1[i])) {
-             console.log('тут');
-            return false;
-        }        
-    }
+//      for (let i = 0; i < arr1.length; i += 1){
+//          if (!arr2.includes(arr1[i])) {
+//              console.log('тут');
+//             return false;
+//         }        
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
 // console.log(compareArrays(arr1, arr2));
 // console.log(compareArrays(arr3, arr4));
