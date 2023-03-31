7db32c0cecc9b0127816c30f4909fd11d4d530f9
olha-smieshko <smeshkoom@gmail.com>
2023-03-31 18:37:47 +0300

    foreach

--- js/blend-3/module-4/for-each-method.js
+++ js/blend-3/module-4/for-each-method.js
@@ -12,12 +12,19 @@
 */
 
 // function logItems(items) {
-//   console.log(items);
+//     console.log(items);
 
-// for (let i = 0; i < items.length; i += 1) {
-//   console.log(`${i + 1} - ${items[i]}`);
+//     for (let i = 0; i < items.length; i += 1) {
+//         console.log(`${i + 1} - ${items[i]}`);
+//     }
 // }
-
+/************** Changed ************/
+// const logItems = items => {
+//     console.log(items);
+//     items.forEach((item, i) => {
+//         console.log(`${i + 1} - ${item}`)
+//     });
+// }
 // logItems(['Mango', 'Poly', 'Ajax']);
 // logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
 
@@ -26,28 +33,49 @@
 */
 
 // function printContactsInfo({ names, phones } = {}) {
-//   const nameList = names.split(',');
-//   const phoneList = phones.split(',');
+//     const nameList = names.split(',');
+//     const phoneList = phones.split(',');
 
-// for (let i = 0; i < nameList.length; i += 1) {
-//   console.log(`${nameList[i]}: ${phoneList[i]}`);
+//     for (let i = 0; i < nameList.length; i += 1) {
+//         console.log(`${nameList[i]}: ${phoneList[i]}`);
+//     }
 // }
+/************** Changed ************/
+// const printContactsInfo = ({ names, phones } = {}) => {
+//     const nameList = names.split(',');
+//     const phoneList = phones.split(',');
+//     nameList.forEach((name, i) => console.log(`${name}: ${phoneList[i]}`))
+// }
+//     printContactsInfo({
+//         names: 'Jacob,William,Solomon,Artemis',
+//         phones: '89001234567,89001112233,890055566377,890055566300',
+//     });
 
-// printContactsInfo({
-//   names: 'Jacob,William,Solomon,Artemis',
-//   phones: '89001234567,89001112233,890055566377,890055566300',
-// });
 
 /*
 ? Виконайте рефакторинг коду, використовуючи метод forEach і стрілочні функції.
 */
 
 // function calсulateAverage(...args) {
-//   let total = 0;
+//     let total = 0;
 
-// for (let i = 0; i < args.length; i+=1) {
-//   total += args[i];
+//     for (let i = 0; i < args.length; i += 1) {
+//         total += args[i] / args.length;
+//     }
+//     return total;
 // }
+/************** Changed ************/
+// const calсulateAverage = (...args) => {
+//     let total = 0;
+//     args.forEach(arg => {
+//         total += arg / args.length;
+//     })
+//     return total;
+// }
+/************** Changed2 ************/
+// const calсulateAverage = (...args) => args.reduce((acc, arg) => acc += arg / args.length ,0)
+
+
 
 // console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
 // console.log(calсulateAverage(14, 8, 2)); // 8
